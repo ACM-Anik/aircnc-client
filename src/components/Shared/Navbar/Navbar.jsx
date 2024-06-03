2236cf2b3084151a578fee7157123ff27e0b445a
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-03 23:33:32 +0600

    Search component added to the Navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from '../Container';
 import Logo from './Logo';
+import Search from './Search';
 
 
 const Navbar = () => {
@@ -10,7 +11,7 @@ const Navbar = () => {
                 <Container>
                     <div className="flex flex-row items-center justify-between gap-3 md:pag-0">
                         <Logo></Logo>
-                        <div>Search</div>
+                        <Search></Search>
                         <div>Menu</div>
                     </div>
                 </Container>
