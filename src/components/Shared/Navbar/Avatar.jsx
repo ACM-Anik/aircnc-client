4416ef73c071edd86df8679a0c9b18daa9fe19fd
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-05 22:57:56 +0600

    Home as a Routes children added, Categories with it's components like categoriesBoxs.jsx and categoriesData.js (for react icon) created and also added to the Home.

--- src/components/Shared/Navbar/Avatar.jsx
+++ src/components/Shared/Navbar/Avatar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import avatarImg from "../../../assets/images/placeholder.jpg";
 import { AuthContext } from '../../../providers/AuthProvider';
 
