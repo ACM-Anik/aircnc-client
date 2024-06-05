00d3f721a47e756056c93c3d1e1f76974d06d9f6
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-05 23:22:07 +0600

    Categories fontend done

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Container from '../Shared/Container';
+import { categories } from "./CategoriesData";
+import CategoryBox from './CategoryBox';
+
 
 const Categories = () => {
     return (
-        <div>cat</div>
+        <Container>
+            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+                {
+                    categories.map((item) => (
+                        <CategoryBox
+                            key={item.label}
+                            label={item.label}
+                            icon={item.icon}
+                        ></CategoryBox>
+                    ))
+                }
+            </div>
+        </Container>
     );
 };
 
