c6c250eea120e3dadfed74085e6374b695438cf2
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-12 00:10:39 +0600

    Error solved at the useeffect while fetching rooms.json data

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Container from '../Shared/Container';
+
 
 const Rooms = () => {
+    const [rooms, setRooms] = useState();
+
+    useEffect(() => {
+        fetch("./rooms.json")
+        .then(res => res.json())
+        .then(data => setRooms(data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            {rooms?.length}
+        </Container>
     );
 };
 
