e77ea7d9bbeea5fc4738a0878f3ec96f4e0b406c
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-12 23:27:56 +0600

    Card component created

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -7,14 +7,26 @@ const Rooms = () => {
 
     useEffect(() => {
         fetch("./rooms.json")
-        .then(res => res.json())
-        .then(data => setRooms(data))
-        .catch(error => console.log(error))
+            .then(res => res.json())
+            .then(data => setRooms(data))
+            .catch(error => console.log(error))
     }, []);
 
     return (
         <Container>
-            {rooms?.length}
+            <div
+                className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+            >
+                {
+                    rooms?.map(room => (
+                        <div
+                            key={room.location}
+                            className="">
+                            {room.location}
+                        </div>
+                    ))
+                }
+            </div>
         </Container>
     );
 };
