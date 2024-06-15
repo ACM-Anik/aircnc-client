09f4726bf17b9e3d258c70a6aaf1dd378b202bcd
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-16 00:07:26 +0600

    Used grid on RoomInfo and Reservation and set the RoomDetails page as PrivateRoute

--- src/Pages/RoomDetails/RoomDetails.jsx
+++ src/Pages/RoomDetails/RoomDetails.jsx
@@ -9,8 +9,10 @@ const RoomDetails = () => {
             <div className="max-w-screen-lg mx-auto">
                 <div className="flex flex-col gap-6">
                     <Header></Header>
-                    <RoomInfo></RoomInfo>
-                    <div className="">Reservation</div>
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <RoomInfo></RoomInfo>
+                        <div>Reservation</div>
+                    </div>
                 </div>
             </div>
         </Container>
