9df9b830833eb3f0ba64985539df75188b1e4235
ACM-Anik <anikmojumder5252@gmail.com>
2024-08-14 23:21:49 +0600

    RoomDataRow.jsx created for adding to dashboard's MyListings. Also needful structure implementd

--- src/Pages/Dashboard/MyListings.jsx
+++ src/Pages/Dashboard/MyListings.jsx
@@ -15,7 +15,7 @@ const MyListings = () => {
     }, [user]);
     
     console.log(rooms);
-    
+
     return (
       <div className='container mx-auto px-4 sm:px-8'>
         <div className='py-8'>
